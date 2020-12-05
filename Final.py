b073cad0c7e7fe1bc175d508753c85d02581d8f7
Hussain Alsaif <hussainfalsaif@gmail.com>
2020-12-05 19:38:34 +0300

    editing Final.py for readability and user-freindlyness

--- Final.py
+++ Final.py
@@ -76,7 +76,7 @@ def load_data(city, month, day):
     return df
 
 
-#The following function displays the most common month for rides, day and hour
+#The follwing function shows the most common month, day and hour for bike usage.
 def time_stats(df):
     # TO DO: display the most common month
     mostc_month = df["month"].mode()
